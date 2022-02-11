cdef8da94dafde6d5a74fe6d451b36b6e16beefb
Phakpoom Ittirattanakomon <phakpoomittirattanakomon@Phakpooms-MacBook-Pro.local>
2022-02-11 20:59:43 +0700

    add redirects

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  serviceUrl: "http://localhost:4000"
+//   serviceUrl: "http://localhost:4000",
+  serviceUrl: "https://mongoose-authen-beeris.herokuapp.com"
 };
 
 /*
